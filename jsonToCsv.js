e0a4ad082d27265589a5ca19229e2933dfa6d5fc
Stephen Amaza <steveamaza@gmail.com>
2023-05-16 12:33:01 +0000

    Create base code for extracting csv from json

--- jsonToCsv.js
+++ jsonToCsv.js
@@ -1,16 +1,36 @@
-const json2csv = require('json2csv').parse;
 const fs = require('fs');
-dotenv = require('dotenv').config();
+require('dotenv').config();
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-// Example JSON object
-const jsonData = [process.env.JSON_RESPONSE];
+// Bring in JSON object
+const jsonData = [];
 
-// Convert JSON to CSV
-const csvData = json2csv(jsonData);
+// Check that your data is represented properly
+console.log(typeof jsonData);
 
-// Write CSV to file
-fs.writeFile('output.csv', csvData, (err) => {
-  if (err) throw err;
-  console.log('CSV file generated successfully!');
-});
+// Define the CSV writer with the desired configuration
+const csvWriter = createCsvWriter({
+    path: 'output.csv',
+    header: [
+      { id: 'title', title: 'Title' },
+      { id: 'id', title: 'ID' },
+      { id: 'position', title: 'Position' },
+      { id: 'url', title: 'URL' },
+      { id: 'created_at', title: 'Created at' },
+      { id: 'updated_at', title: 'Updated at' },
+      { id: 'description', title: 'Description' }
+    ]
+  });
+  
+  try {
+    if (jsonData.length > 0) {
+      csvWriter
+        .writeRecords(jsonData)
+        .then(() => console.log('CSV file created successfully!'));
+    } else {
+      console.log('jsonData is empty. No data to write to CSV.');
+    }
+  } catch (err) {
+    console.error(err);
+  }
