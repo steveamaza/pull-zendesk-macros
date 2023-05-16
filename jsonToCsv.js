4b66506b5b4f71a4821a8f994d6d0bf6e1674527
Stephen Amaza <steveamaza@gmail.com>
2023-05-16 17:20:55 +0000

    Make API calls instead of uploading values directly

--- jsonToCsv.js
+++ jsonToCsv.js
@@ -2,35 +2,73 @@ const fs = require('fs');
 require('dotenv').config();
 
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const request = require('request-promise');
 
-// Bring in JSON object
-const jsonData = [];
+var jsonData = [];
+var api_url = 'https://paystack.zendesk.com/api/v2/macros/active.json?active=true';
 
-// Check that your data is represented properly
-console.log(typeof jsonData);
+async function fetchData() {
+  var count = 0;
 
-// Define the CSV writer with the desired configuration
-const csvWriter = createCsvWriter({
-    path: 'output.csv',
-    header: [
-      { id: 'title', title: 'Title' },
-      { id: 'id', title: 'ID' },
-      { id: 'position', title: 'Position' },
-      { id: 'url', title: 'URL' },
-      { id: 'created_at', title: 'Created at' },
-      { id: 'updated_at', title: 'Updated at' },
-      { id: 'description', title: 'Description' }
-    ]
-  });
+  do {
+    const options = {
+      method: 'GET',
+      url: api_url,
+      headers: {
+        'Authorization': 'Basic ' + process.env.ZENDESK_TOKEN,
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const response = await request(options);
+      jsonData.push(JSON.parse(response).macros); // Assign the response to jsonData
+      api_url = JSON.parse(response).next_page;
+    } catch (error) {
+      throw new Error(error);
+    }
+  } while (api_url !== null);
   
+  return jsonData;
+}
+
+// Call fetchData() and handle the result
+fetchData()
+  .then((result) => {
+    // Use the updated jsonData outside the fetchData() function
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+
+async function writeDataToCsv() {
   try {
+    const jsonData = await fetchData();
+
     if (jsonData.length > 0) {
-      csvWriter
-        .writeRecords(jsonData)
-        .then(() => console.log('CSV file created successfully!'));
+      const csvWriter = createCsvWriter({
+        path: 'output.csv',
+        header: [
+          { id: 'title', title: 'Title' },
+          { id: 'id', title: 'ID' },
+          { id: 'position', title: 'Position' },
+          { id: 'url', title: 'URL' },
+          { id: 'created_at', title: 'Created at' },
+          { id: 'updated_at', title: 'Updated at' },
+          { id: 'description', title: 'Description' }
+        ]
+      });
+
+      await csvWriter.writeRecords(jsonData);
+      console.log('CSV file created successfully!');
     } else {
       console.log('jsonData is empty. No data to write to CSV.');
     }
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
   }
+}
+
+writeDataToCsv();
